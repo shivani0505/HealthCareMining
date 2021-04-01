6538f61c5018b33caf6cedc1cdc543b08c9dd279
shivani0505 <69835737+shivani0505@users.noreply.github.com>
2021-04-01 16:21:38 -0700

    Healthcare Mining.
    Healthcare Mining Initial files.

---
+++ Indexing/index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+	<title>Search</title>
+</head>
+
+<body>
+	<h1>Search</h1>
+	<form action="search.php" method="get">
+		<label>Query</label>
+		<input type="text" size="30" name="query" value="*:*" /><br><br>
+		<input type="submit" />
+	</form>
+</body>
+
+</html>
