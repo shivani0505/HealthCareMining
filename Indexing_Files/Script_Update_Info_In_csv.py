43206c335121c2af92f05eee3300aa606e48d344
shivani0505 <69835737+shivani0505@users.noreply.github.com>
2021-04-02 15:08:51 -0700

    Extra information Bug solved.

--- Indexing_Files/Script_Update_Info_In_csv.py
+++ Indexing_Files/Script_Update_Info_In_csv.py
@@ -10,15 +10,13 @@ files = [f for f in os.listdir('.') if os.path.isfile(f) and ("cahmx.csv" in f o
 with open('/Users/shivanipriya/Documents/GitHub/HealthCareMining/ScraperFiles/stage1_scrapping.json') as scraper_file:
 	scraper_data = json.load(scraper_file)
 
-new_df = pandas.DataFrame(columns =["url","Title","Author","Total_Replies","Total_Weight"])
-
 replies_weight = 50
 date_weight = 50
 
 for file in files:
 	print(file)
 	df = pandas.read_csv(file)
-
+	new_df = pandas.DataFrame(columns =["url","Title","Author","Total_Replies","Total_Weight"])
 	for item in df.itertuples():
 		PostNum = item.PostNumber
 		link = item.PostLink
@@ -29,7 +27,6 @@ for file in files:
 				subreplies_count = 0
 				Total_Weight = 0
 				Total_Replies = 0
-				
 
 				Title = keyValue["title"]
 				Author = keyValue["author"]
@@ -37,13 +34,13 @@ for file in files:
 				no_of_replies = len(replies)
 				
 				while replies_index<no_of_replies:
-					subreplies = replies[i]['sub_replies']
+					subreplies = replies[replies_index]['sub_replies']
 					replies_index = replies_index + 1
 					if '' not in subreplies:
 						subreplies_count = subreplies_count + 1
 				#Total_Replies is the summation of number of replies and sub_replies on a post.
 				#Higher number denotes more user engaging in the post.
-				Total_Replies = subreplies_count + length
+				Total_Replies = subreplies_count + no_of_replies
 				#Total_Weight determines the rank of the post in the search result.
 				#Higher weight is shown above in the search result list.
 				if(Total_Replies != 0):
@@ -56,3 +53,8 @@ for file in files:
 	df3 = df3.drop(["url"],axis =1)
 	filename ="updated_" + file
 	df3.to_csv(filename,index=False)
+	del df3
+	del new_df
+	del df
+	#df3 = df3.drop(labels=["PostNumber","SymptomName","PostLink","Title","Author","Total_Replies","Total_Weight"],axis = 1)
+	
